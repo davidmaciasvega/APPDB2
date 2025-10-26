38e6f29488759b8fb852a744c57511662a552051
David <david.macias1354@alumnos.udg.mx>
2025-10-26 12:29:14 -0600

    Actualizar db.js

--- backend/db.js
+++ backend/db.js
@@ -8,4 +8,7 @@ export const pool = new pg.Pool({
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
   port: process.env.PGPORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
